b0a1b2f225ffa6c6e625673ab620f3fc806ee8b2
Cas de Bruijn <71756854+CasdeBruijnEHB@users.noreply.github.com>
2023-05-26 15:23:22 +0200

    feat: add animated textured gltfs and environment

--- finalwork/app/page.tsx
+++ finalwork/app/page.tsx
@@ -2,9 +2,17 @@
 import { createRoot } from "react-dom/client";
 import React, { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 import Floor from "@/components/floor";
+import { Computer } from "@/components/computer";
+import { Environment, OrthographicCamera } from "@react-three/drei";
+import { Suspense } from "react";
+import { Stats, OrbitControls, Lightformer } from "@react-three/drei";
+import { ComputerTwee } from "@/components/Scene";
 
 export default function Home() {
+  const deg2rad = (degrees: number) => degrees * (Math.PI / 180);
+
   return (
     <main>
       <div className="scene">
@@ -12,11 +20,22 @@ export default function Home() {
           shadows
           className="canvas"
           camera={{
-            position: [-6, 7, 7],
+            position: [-10, 7, 12],
+            rotation: [deg2rad(-20), 0, 0],
           }}
+          style={{ background: "white" }}
         >
-          <ambientLight color={"white"} intensity={0.3} />
-          <Floor />
+          <Environment
+            files="https://cdn.jsdelivr.net/gh/Sean-Bradley/React-Three-Fiber-Boilerplate@environment/public/img/venice_sunset_1k.hdr"
+            background
+          />
+          <Suspense fallback={null}>
+            <ambientLight color={"white"} intensity={0.5} />
+            <Computer />
+            <gridHelper args={[10, 10, `white`, `gray`]} />
+
+            <Stats />
+          </Suspense>
         </Canvas>
       </div>
     </main>
